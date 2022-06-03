88103685de8e5e720bf5b65d5d502c9220bf36aa
Eunji Nam <godangel31@gmail.com>
2022-06-03 09:42:50 +0900

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,23 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>Hello {{ animal }}</h1>
+  <h2>원숭이는 {{ food }}를 좋아합니다.</h2>
+  <input type="text" v-model="food" />
+  <a :href="food">{{food}}</a>
+  <hr />
+  <h2 v-bind:class="{ red : food==='apple' , 'not-good' : food==='rice' }">원숭이는 {{food}}를 좋아합니다. </h2>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data() { 
+    return {
+      // key: "value",
+      animal: "Monkey",
+      food: "apple",
+      imageSource: "https://placeimg.com/100/100/any",
+      naverUrl: "https://www.naver.com"
+    }
   }
 }
 </script>
@@ -22,5 +30,21 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+input {
+  font-size: 20px;
+}
+
+.orange {
+  color: orange;
+}
+.salmon {
+  color: salmon;
+}
+.red {
+  color: red;
+}
+.not-good {
+  text-decoration: line-through;
 }
 </style>
